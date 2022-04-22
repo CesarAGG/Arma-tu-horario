2fc6002ff68c466675bd5a1d446915afaef88359
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2022-04-22 13:30:35 -0500

    Añadir archivo que define la clase Course

---
+++ src/course.ts
@@ -0,0 +1,40 @@
+export class Course {
+    title: string;
+    professor: string;
+    schedule: ClassSchedule;
+
+    constructor(title: string, professor: string, schedule: ClassSchedule) {
+        this.title = title;
+        this.professor = professor;
+        this.schedule = schedule;
+    }
+
+    setSchedule(day: Day, startTime: Time, endTime: Time) {
+        this.schedule[day] = { startTime, endTime };
+    }
+
+    logSchedule() {
+        console.log(this.schedule);
+    }
+
+}
+
+type HH = '00' | '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23';
+type MM = '00' | '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' | '32' | '33' | '34' | '35' | '36' | '37' | '38' | '39' | '40' | '41' | '42' | '43' | '44' | '45' | '46' | '47' | '48' | '49' | '50' | '51' | '52' | '53' | '54' | '55' | '56' | '57' | '58' | '59';
+
+type Day = 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday';
+type Time = `${HH}:${MM}`;
+
+interface Session {
+    startTime: Time;
+    endTime: Time;
+}
+
+interface ClassSchedule {
+    monday: Session;
+    tuesday: Session;
+    wednesday: Session;
+    thursday: Session;
+    friday: Session;
+    saturday: Session;
+}
