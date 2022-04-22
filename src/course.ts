521904ad5bc230bf3f108141d9275f7e976a2d48
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2022-04-22 17:45:50 -0500

    agregar metodo estatico y cambiado nombre de un campo

--- src/course.ts
+++ src/course.ts
@@ -1,20 +1,39 @@
 export class Course {
     title: string;
     professor: string;
-    schedule: IClassSchedule;
+    classSchedule: IClassSchedule;
 
     constructor(title: string, professor: string, schedule: IClassSchedule) {
         this.title = title;
         this.professor = professor;
-        this.schedule = schedule;
+        this.classSchedule = schedule;
+    }
+
+    /**
+     * If the class starts and ends at the same time every day, this can
+     * be used to make the code more readable.
+     * @param days array of days in which the class should be scheduled
+     * @param session start and end time of the class
+     * @returns an instance of the interface IClassSchedule
+     */
+    static classScheduleFromDays(days: Day[], session: ISession) {
+        let schedule: IClassSchedule = {
+            "monday": days.includes("monday") ? session : undefined,
+            "tuesday": days.includes("tuesday") ? session : undefined,
+            "wednesday": days.includes("wednesday") ? session : undefined,
+            "thursday": days.includes("thursday") ? session : undefined,
+            "friday": days.includes("friday") ? session : undefined,
+            "saturday": days.includes("saturday") ? session : undefined,
+        };
+        return schedule;
     }
 
     setSession(day: Day, session: ISession) {
-        this.schedule[day] = session;
+        this.classSchedule[day] = session;
     }
 
     getSession(day: Day): ISession {
-        return this.schedule[day];
+        return this.classSchedule[day];
     }
 
 }
