5cea95152fd9e6cd6eb16998447d3f2c2890d1b3
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2022-04-22 18:46:58 -0500

    loggea en la consola si se elimina un curso

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,4 +1,4 @@
-import { Course, } from "./course";
+import { Course, Day } from "./course";
 
 export class Schedule {
     courses: Course[];
@@ -6,10 +6,27 @@ export class Schedule {
     constructor(courses: Course[]) {
         this.courses = courses;
         // Courses with the same title are not allowed so they must be filtered out.
-        this.courses = this.courses.filter((course, index, self) => self.findIndex(c => c.title === course.title) === index);
+        // The console logs the course that was filtered out.
+        this.courses = this.courses.filter((course, index, self) => {
+            let courseTitles = self.map(c => c.title);
+            if (courseTitles.indexOf(course.title) !== index) {
+                logFilterOut(course, "Duplicate title");
+                return false;
+            }
+            return true;
+        });
     }
 
     getCoursesJSON(): any {
         return JSON.stringify(this.courses);
+    }
+
+}
+
+function logFilterOut(course: Course, reason: string): void {
+    console.log(`Course ${course.title} from professor ${course.professor} was filtered out. Reason: ${reason}. The schedule was:`);
+    let days: Day[] = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+    for (let day of days) {
+        console.log(course.getSession(day) ? `${day}: ${course.getSession(day).startTime} - ${course.getSession(day).endTime}, ` : "");
     }
 }
