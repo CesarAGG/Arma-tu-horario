4340b944eca77ac6bcea6333af45249592b038c9
CesarAGG <71679922+CesarAGG@users.noreply.github.com>
2022-04-24 20:31:23 -0500

    x2

--- src/scores.ts
+++ src/scores.ts
@@ -12,19 +12,19 @@ export abstract class compactScore {
      * @param schedule Schedule from which the score is calculated
      * @returns compactScore (from 0 to 1 inclusive)
      */
-    static getScore(schedule: Schedule): number {
-        let score = 0;
+    static getScore(schedule: Schedule): string {
+        let score = 1;
         for (let daySession of schedule.allDaySessions) {
             let startTime = parseTime(daySession.startTime);
             let endTime = parseTime(daySession.endTime);
             let timeDiff = endTime - startTime;
-            score += timeDiff
+            score -= timeDiff / 7 / 1440;
         }
 
         let activeDays = schedule.activeDays;
         let activeDaysModifier = MODIFIER_VALUE + (1 - MODIFIER_VALUE) * (activeDays / 6);
-        score = score / (activeDaysModifier * 1440);
-        score = Math.round((score + Number.EPSILON) * 100000) / 100000
-        return score;
+        score = score / activeDaysModifier;
+        score = Math.round((score * 100 + Number.EPSILON) * 1000) / 1000
+        return score + "%";
     }
 }
